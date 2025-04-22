716a643b8b6d229e184b4152568a8ede6496835d
IanGWhite <97561180+IanGWhite@users.noreply.github.com>
2025-04-22 11:33:00 -0500

    added animations and re-organized the main screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'my_game.dart';
+import 'package:rive/rive.dart' as myRize;
 
 
 class HomeScreen extends StatefulWidget {
@@ -15,38 +16,53 @@ class HomeScreen extends StatefulWidget {
 
 
 class _HomeScreenState extends State<HomeScreen> {
+String animationName = 'Smile';
 
+void _smilePlease(){
+  setState((){
+    if(animationName == 'Smile'){
+      animationName = 'Frown';
+    } else {
+      animationName = 'Smile';
+      
+    }
+  });
+}
 
-
-
-
-
-
-
-
-
-
-  
-  void gameOver() => Navigator.of(context).pop();
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          GestureDetector(
-            child: Image.asset('assets/BACK.png'),
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
+void changePage(){
+                Navigator.of(context).push(MaterialPageRoute(
                 builder: (context) => GameWidget(
                     game: MyGame(
                   onGameOver: gameOver,
                 )),
               ));
-            },
+}
+
+
+  void gameOver() => Navigator.of(context).pop();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(actions: [IconButton(
+                  onPressed: () => changePage(), 
+                  tooltip: "Erase Tasks", 
+                  icon: const Icon(Icons.casino)
+                  )],),
+      body: Center(
+        child: Container(
+        width: 400,
+        height: 400,
+            child: myRize.RiveAnimation.asset('assets/smiley(3).riv', alignment: Alignment.center,
+            fit: BoxFit.contain,
+          animations: [animationName, 'Frown'],),
           ),
-        ],
       ),
+      
+         floatingActionButton: FloatingActionButton(
+        onPressed: _smilePlease,
+        tooltip: 'Increment',
+        child: Icon(Icons.add)),
+      
     );
   }
 }
